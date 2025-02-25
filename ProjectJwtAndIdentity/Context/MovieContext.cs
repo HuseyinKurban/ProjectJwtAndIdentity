6a596f29fb05a883fa03a5c8ea6403ab30190561
Hüseyin Kurban <huseyinkurban03@gmail.com>
2025-02-25 22:12:01 +0300

    Add project files.

---
+++ ProjectJwtAndIdentity/Context/MovieContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProjectJwtAndIdentity.Entities;
+
+namespace ProjectJwtAndIdentity.Context
+{
+    public class MovieContext:IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-32Q9FH5;initial catalog=IdentityMovieDb;integrated security=true;TrustServerCertificate=true");
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ ProjectJwtAndIdentity/Controllers/RoleController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectJwtAndIdentity.Entities;
+using ProjectJwtAndIdentity.Models;
+
+namespace ProjectJwtAndIdentity.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+        private readonly UserManager<AppUser> _userManager;
+
+        public RoleController(RoleManager<AppRole> roleManager, UserManager<AppUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult RoleList()
+        {
+            var values = _roleManager.Roles.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            AppRole appRole = new AppRole()
+            {
+                Name = model.RoleName
+            };
+            await _roleManager.CreateAsync(appRole);
+            return RedirectToAction("RoleList");
+        }
+
+        public async Task<IActionResult> DeleteRole(int id)
+        {
+            var value =await _roleManager.Roles.FirstOrDefaultAsync(x=>x.Id==id);
+            await _roleManager.DeleteAsync(value);
+            return RedirectToAction("RoleList");
+        }
+
+        public IActionResult UserList()
+        {
+            var values = _userManager.Users.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AssignRole(int id)
+        {
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.Id == id);
+            TempData["x"] = user.Id;
+            var roles = _roleManager.Roles.ToList();
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            List<RoleAssignViewModel> roleAssignViewModels = new List<RoleAssignViewModel>();
+            foreach (var item in roles)
+            {
+                RoleAssignViewModel model = new RoleAssignViewModel();
+                model.RoleId = item.Id;
+                model.RoleName = item.Name;
+                model.RoleExist = userRoles.Contains(item.Name);
+                roleAssignViewModels.Add(model);
+
+            }
+
+            return View(roleAssignViewModels);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AssignRole(List<RoleAssignViewModel> model)
+        {
+            return View();
+        }
+    }
+}
