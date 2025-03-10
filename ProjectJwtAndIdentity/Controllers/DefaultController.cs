5f075a8b4954d5eb05e7965b5259cb38013c97ff
Hüseyin Kurban <huseyinkurban03@gmail.com>
2025-03-10 15:27:31 +0300

    Add project files.

---
+++ ProjectJwtAndIdentity/Controllers/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectJwtAndIdentity.JWTTools;
+using ProjectJwtAndIdentity.Models;
+
+namespace ProjectJwtAndIdentity.Controllers
+{
+    public class DefaultController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(ResultAppUser resultAppUser)
+        {
+
+            var value = JwtTokenGenerator.GenerateToken(resultAppUser);
+            ViewBag.v = value;
+            return View();
+        }
+    }
+}
